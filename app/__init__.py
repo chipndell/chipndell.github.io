ebc5f9a741a44da3eef49f443ce1ff21de64b0a9
Manan Bhavsar <mananbh9@live.com>
2020-08-21 23:39:11 -0400

    After change

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,3 @@ from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
-
-
-# from flask_mail import Mail
-# from flask_bootstrap import Bootstrap
-
-
-# Bootstrap(app)
-# mail = Mail(app)
-# form = FeedbackForm()
-
-
-if __name__ == "__main__":
-	app.run(debug=True)
-
--- personal_web.py
+++ personal_web.py
@@ -1,2 +1,12 @@
-from app import app
+from flask import Flask, render_template
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yfwqfbwjeacwkleavn bwrfbqwlfwenfqwefqwf'
+
+if __name__ == "__main__":
+	app.run()
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
