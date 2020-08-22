d4d5504979c006c0d282af6c80977f9e09e08250
Manan Bhavsar <mananbh9@live.com>
2020-08-21 23:52:25 -0400

    app.py file renamed

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yfwqfbwjeacwkleavn bwrfbqwlfwenfqwefqwf'
+
+if __name__ == "__main__":
+	app.run()
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
